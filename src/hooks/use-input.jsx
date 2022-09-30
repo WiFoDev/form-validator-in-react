609d0063bf333109cce8a8f43370b5e63750d55e
WiFoDev <lou81804@gmail.com>
2022-09-30 17:27:58 -0500

    manage state with useReducer

--- src/hooks/use-input.jsx
+++ src/hooks/use-input.jsx
@@ -1,28 +1,49 @@
-import { useState } from 'react'
+import { useReducer } from 'react'
+
+const defaultState = {
+  value: '',
+  touched: false,
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'Change':
+      return {
+        value: action.value,
+        touched: false,
+      }
+    case 'Blur':
+      return {
+        value: state.value,
+        touched: true,
+      }
+    case 'Reset':
+      return defaultState
+  }
+
+  return defaultState
+}
 
 export const useInput = (validate) => {
-  const [value, setValue] = useState('')
-  const [touched, setTouched] = useState(false)
+  const [state, dispatch] = useReducer(reducer, defaultState)
 
-  const isValueValid = validate(value)
-  const hasError = !isValueValid && touched
+  const isValueValid = validate(state.value)
+  const hasError = !isValueValid && state.touched
 
   const valueChangeHandler = (evt) => {
-    setTouched(false)
-    setValue(evt.target.value)
+    dispatch({ type: 'Change', value: evt.target.value })
   }
 
   const inputTouchedHandler = () => {
-    setTouched(true)
+    dispatch({ type: 'Blur' })
   }
 
   const reset = () => {
-    setValue('')
-    setTouched(false)
+    dispatch({ type: 'Reset' })
   }
 
   return {
-    value,
+    value: state.value,
     hasError,
     valueChangeHandler,
     inputTouchedHandler,
