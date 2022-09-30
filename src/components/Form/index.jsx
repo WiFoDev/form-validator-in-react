06c4489760530b7265fae77657d1f7ca66558e88
WiFoDev <lou81804@gmail.com>
2022-09-30 12:27:16 -0500

    add input and submition handlers

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,16 +1,65 @@
+import { useState } from 'react'
+
 import { form, input, button, label } from './styles.module.css'
 
+const DEFAULT_FORM_STATE = {
+  name: '',
+  email: '',
+}
+
 export const Form = () => {
+  const [userInputs, setUserInputs] = useState(DEFAULT_FORM_STATE)
+
+  const nameChangeHandler = (evt) => {
+    setUserInputs((prevState) => {
+      return {
+        ...prevState,
+        name: evt.target.value,
+      }
+    })
+  }
+
+  const emailChangeHandler = (evt) => {
+    setUserInputs((prevState) => {
+      return {
+        ...prevState,
+        email: evt.target.value,
+      }
+    })
+  }
+  const submitFormHandler = (evt) => {
+    evt.preventDefault()
+    const { name, email } = userInputs
+    const newUser = {
+      name,
+      email,
+    }
+
+    setUserInputs(DEFAULT_FORM_STATE)
+  }
+
   return (
-    <form className={form}>
+    <form className={form} onSubmit={submitFormHandler}>
       <label className={label} htmlFor="name">
         Your Name
       </label>
-      <input className={input} id="name" type="text" />
+      <input
+        className={input}
+        id="name"
+        type="text"
+        value={userInputs.name}
+        onChange={nameChangeHandler}
+      />
       <label className={label} htmlFor="email">
         Your Email
       </label>
-      <input className={input} id="email" type="text" />
+      <input
+        className={input}
+        id="email"
+        type="text"
+        value={userInputs.email}
+        onChange={emailChangeHandler}
+      />
       <button className={button}>Submit</button>
     </form>
   )
